8fd2cd1348b2fa8a4775291fc8b7f34c7252f8c9
RayYangFromOrderly <ray.yang@ezorderly.com>
2022-03-24 16:29:35 +0800

    upgrade

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='media-ext',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/orderly-com/media-ext',
     license='BSD',
     description='CDP extension for media industry',
