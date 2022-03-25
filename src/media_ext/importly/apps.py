4eed7dc9e578c562fb0d1cf1d06d4f0cd27cf9ee
RayYangFromOrderly <ray.yang@ezorderly.com>
2022-03-25 14:00:10 +0800

    add app

---
+++ src/media_ext/importly/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ImportlyConfig(AppConfig):
+    name = 'media_ext.importly'
---
+++ src/media_ext/media/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class MediaConfig(AppConfig):
+    name = 'media_ext.media'
