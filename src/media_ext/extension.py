cbb38f2f1a5257ed105cbeb01f0ee68927f867c8
RayYangFromOrderly <ray.yang@ezorderly.com>
2022-04-14 15:22:16 +0800

    fix readbase list

--- src/media_ext/extension.py
+++ src/media_ext/extension.py
@@ -1,5 +1,8 @@
 from extension.extension import Extension
 
+from django.db.models import Count, Min, Sum, Max, F, Func, Value, CharField, Q
+from django.db.models.functions import Extract
+
 class MediaExtension(Extension):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,6 +18,37 @@ class MediaExtension(Extension):
             return function
 
         return registry
+
+    def get_clientbase_behaviors(clientbase):
+        behaviors = []
+
+        read_qs = (clientbase.readbase_set.filter(removed=False)
+            .values('datetime__date', 'articlebase__title', 'articlebase__path')
+            .annotate(from_datetime=Min('datetime'), to_datetime=Max('datetime'))
+        ).order_by('from_datetime')
+
+        for item in read_qs:
+            from_datetime = item['from_datetime']
+            to_datetime = item['to_datetime']
+            value = item['articlebase__title']
+            path = item['articlebase__path']
+            if not value:
+                value = '--'
+
+            if path:
+                value = f'<a class="text-info" href="{path}">{value}</a>'
+
+
+            obj = {
+                'datetime': f'{from_datetime}~{to_datetime}',
+                'trigger_by': 'client',
+                'action': '閱讀',
+                'value': value,
+            }
+            behaviors.append(obj)
+        return behaviors
+
+
 
 media_ext = MediaExtension()
 
