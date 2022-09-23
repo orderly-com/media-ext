6bbb19096804fbd1b2c46701f0df6a7cc5bbbba3
Ray <ray.yang@ezorderly.com>
2022-09-23 16:38:41 +0800

    [feat] add index to datetime of readbases

--- src/media_ext/extension.py
+++ src/media_ext/extension.py
@@ -14,7 +14,7 @@ class MediaExtension(Extension):
         return {
             'readbases':
             {
-                'indexes': ['cid', 'progress', 'clientbase_id', 'productbase_id'],
+                'indexes': ['cid', 'progress', 'clientbase_id', 'productbase_id', 'datetime'],
                 'schema': {
                     'datetime': {'type': 'datetime'},
                     'events.datetime': {'type': 'datetime'}
